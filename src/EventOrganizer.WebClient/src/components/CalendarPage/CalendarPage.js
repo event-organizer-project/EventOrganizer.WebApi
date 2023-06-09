39b3375d42b5d214bd5be13005d0c20a810c3b74
nuktinov <47114962+nuktinov@users.noreply.github.com>
2023-06-09 05:14:32 +0200

    MM-24: Move Client App (UI) to separate repository (#2)

--- src/EventOrganizer.WebClient/src/components/CalendarPage/CalendarPage.js
+++
@@ -1,8 +0,0 @@
-export default function CalendarPage() {
-    return (
-        <div>
-            <h3>There will be a page with calendar.</h3>
-            <img src='/logo512.png' alt='Calendar Page' />
-        </div>
-    )
-}
