7b5fdf1f90b69101573d9cfc6f630195cba6b367
Mikita Yurtsevich <nuktinov@gmail.com>
2023-06-13 23:02:58 +0200

    MM-24: Move Client App (UI) to separate repository

--- src/EventOrganizer.WebClient/src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer () {
-    return (
-        <footer>
-            <label>Midle Mastery Project</label>
-            <h4>Event Organizer</h4>
-            <label>By Mikita Yurtsevich</label>
-        </footer>
-    )
-}
